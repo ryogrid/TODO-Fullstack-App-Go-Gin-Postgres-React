7a50d6d4532360b35edbfa6cd557172589310646
Ryo Kanbayashi <ryo.contact@gmail.com>
2022-06-04 21:40:24 +0900

    rebuiled frontend static files for modification of url to match one of deployed heroku app.

---
+++ backend/dist/precache-manifest.e8505756a9e0277b14491d9094baa8ec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "814d70e5f32b39a69ed1f0944fdd1084",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ee9546d7b45aad8828e3",
+    "url": "/static/css/main.1ec6c797.chunk.css"
+  },
+  {
+    "revision": "e49c5017ff5778d1b97b",
+    "url": "/static/js/2.3de6b9f2.chunk.js"
+  },
+  {
+    "revision": "ee9546d7b45aad8828e3",
+    "url": "/static/js/main.dde2c1f3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);

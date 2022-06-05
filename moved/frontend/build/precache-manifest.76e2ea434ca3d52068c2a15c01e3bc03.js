67e515112c88872616f8ef01345506dff31e4113
Ryo Kanbayashi <ryo.contact@gmail.com>
2022-06-05 12:01:51 +0900

    implemented auto reload and coded for local development. (4)

---
+++ moved/frontend/build/precache-manifest.76e2ea434ca3d52068c2a15c01e3bc03.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4408413b267b9538147519b1b7ba1c5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dbdb3fbb2b586d1612fe",
+    "url": "/static/css/main.1ec6c797.chunk.css"
+  },
+  {
+    "revision": "e49c5017ff5778d1b97b",
+    "url": "/static/js/2.3de6b9f2.chunk.js"
+  },
+  {
+    "revision": "dbdb3fbb2b586d1612fe",
+    "url": "/static/js/main.c1c1a39b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
